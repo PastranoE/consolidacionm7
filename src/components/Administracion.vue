84a6d4c593f730781978a3f874fdf5b30294a001
pastranoE <eli.pastranosta@gmail.com>
2023-08-19 11:53:23 -0400

    editar y eliminar listo

--- src/components/Administracion.vue
+++ src/components/Administracion.vue
@@ -136,37 +136,9 @@ export default {
             console.log(item)
             this.cursoEdit = item
         },
-        agregarcurso() {
-            this.cursos.push
-        },
-        handleOk(bvModalEvent) {
-            console.log("hola")
-            // Prevent modal from closing
-            bvModalEvent.preventDefault()
-            // Trigger submit handler
-            this.handleSubmit()
-        },
-        checkFormValidity() {
-        const valid = this.$refs.form.checkValidity()
-        this.nameState = valid
-        return valid
-      },
-      resetModal() {
-        this.name = ''
-        this.nameState = null
-      },
-        handleSubmit() {
-        // Exit when the form isn't valid
-        if (!this.checkFormValidity()) {
-          return
-        }
-        // Push the name to submitted names
-        this.submittedNames.push(this.name)
-        // Hide the modal manually
-        this.$nextTick(() => {
-          this.$bvModal.hide('modal-prevent-closing')
-        })
-      }
+        // agregarcurso() {
+        //     this.cursos.push()
+        // },
 
     },
 }
